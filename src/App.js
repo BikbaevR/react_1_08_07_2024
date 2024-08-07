69a9f05a15ea75b4eadecacfd1aecd024ddd3a28
BikbaevR <rafael.bikbayev@gmail.com>
2024-08-07 21:46:54 +0500

    init lesson 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,69 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const name = "Рафаэль"
+    const surname = 'Бикбаев'
+
+    const userCard = 'UserCard'
+    const title = 'title'
+    const mainInfo = 'mainInfo'
+
+    const forTitle = 'Карточка пользователя'
+    const app = 'App'
+
+    return (
+    <div className={app}>
+      <div className={userCard}>
+        <div className={title}>{forTitle}</div>
+          <hr/>
+          <div className={mainInfo}>
+              <p>Имя: {name}</p>
+              <p>Фамилия: {surname}</p>
+          </div>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
+
+
+export const Second = () => {
+
+    const forReverse = 'Строка для реверса'
+    const Style = {
+        color: 'white',
+    }
+    let date = new Date().getDate();
+    let days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
+    // console.log(new Date().getMonth() + new Date().getFullYear())
+
+    let month = new Date().getMonth().toString()
+    let year = new Date().getFullYear().toString()
+
+    let day = days[new Date().getDay()]
+    let dateStr = date.toString()
+
+    if(date % 2 === 0){
+        return (
+            <div style={Style}>
+                <div className="date">
+                    месяц: {month}<br />
+                    год: {year}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div style={Style}>
+                <div className="date">
+                    день недели: {day}<br/>
+                    день месяца: {dateStr}
+                </div>
+            </div>
+        )
+    }
+
+
+}
